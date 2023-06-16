566f425ef258c281e2f8cb2c876902c69324f1ff
Sourabh Niyogi <sourabh@colorfulnotion.com>
2023-06-16 02:00:08 +0000

    ABI-less Decoding of Calls + Event
    This PR contains new BigQuery UDFs to decode EVM calls and events **without knowing an ABI** using 2 key functions:
    
    * `abi_functions.PARSE_CALL(input)`
    * `abi_functions.PARSE_EVENT(data, topics)`
    
    To support ABI-less decoding, each of the above function uses a large ~1MB library containing 2 compressed maps, one covering known call
    methods and known event topics.  Using the call 4byte methodID or the event's topic[0], the most likely ABI is synthesized from the map and
    decoding is attempted.  For events, given the number of topics observed and the ABI guess, any mismatch results in combinatorial
    search on all possible indexed combinations, with the first success considered valid.
    
    How It Works:
    * Big Picture: approach taken in `PARSE_CALL` and `PARSE_EVENT` is to build a 1MB library of 2 _compressed_ maps: `call_map` and `event_map`
    * A `signatures` table is compiled by aggregating ABI signatures with 2.8MM+ records from multiple open-source repos.  However most of these hex signatures have never bee observed on chain.
    * So, we tally _actual_ observations (`numObservations` from `logs` and `transactions`) and presence in ABI contracts (`numContracts` from `contracts`)
    in `crypto_ethereum.{` EVM Chains to build a reduced dataset

--- sql/PARSE_CALL.sql
+++ sql/PARSE_CALL.sql
@@ -1,35 +1,46 @@
-CREATE OR REPLACE FUNCTION `abi_functions`.PARSE_CALL(abi STRING, dat STRING) RETURNS ARRAY<STRUCT<arg STRING, val STRING>> LANGUAGE js AS """
-  abi = JSON.parse(abi);
-  var interface_instance = new ethers.utils.Interface(abi);
-  txargs = [];
-  txtypes = [];
-  res = [];
-
-  // try-catch - input may be malformed.
-  try {
-    var pt = interface_instance.parseTransaction({data: dat});
-
-    frag = "";
-    abi.forEach(function(x){
-      if (x.name == pt.name) {
-        frag = x;
-      }
-    });
-    txvals = pt.args;
-    //txtypes[pt.name]
-    frag.inputs.forEach(function(x) {
-      txargs.push(x.name);
-      txtypes.push(x.type);
-    });
-    for (i = 0; i < txvals.length; i++) {
-      val = txvals[i];
-      if (txtypes[i] == 'address') {
-        val = val.toLowerCase();
-
-      }
-      res.push({arg:txargs[i],val:val});
-    }
-  } catch(e) {}
-  return res;
+CREATE OR REPLACE FUNCTION `substrate-etl.abi_functions.PARSE_CALL`(data STRING) 
+RETURNS ARRAY<STRUCT<name STRING, value STRING>> 
+LANGUAGE js AS 
 """
-OPTIONS(library="gs://blockchain-etl-bigquery/ethers.js");
+function convertBN(val) {
+    if (Array.isArray(val)) {
+        for (let i = 0; i < val.length; i++) {
+            val[i] = convertBN(val[i])
+	}
+    } else if ( val._isBigNumber ) {
+	return val.toString();
+    } 
+    return val;
+}
+
+try {
+  let abiencoded = cn_calls[data.substring(0,10)];
+  if ( abiencoded ) {
+    let pieces = abiencoded.split("|");
+    if ( pieces.length >= 3 ) {
+      let functionName = pieces[0];
+      // "stateMutability":"nonpayable"
+      let abi = {"name":functionName, "type":"function","inputs":[], "outputs":[], "stateMutability":"nonpayable"};
+      let flds = pieces[1].length > 0 ? pieces[1].split(",") : [];
+      let types_unindexed = pieces[2].length > 0 ? pieces[2].split(",") : [];
+      for (let idx = 0; idx < flds.length; idx++) { 
+ 	abi.inputs.push({name: flds[idx], type: types_unindexed[idx] });
+      }
+      const iface = new ethers.utils.Interface([abi]);
+      const decodedData = iface.parseTransaction({ data });
+      return(abi.inputs.map(function(y, idx) {
+        let v = decodedData.args[idx];
+        return({name: y.name, value: convertBN(v) });
+      }));
+    }
+  }
+} catch (err) {
+    return [{name: "err", value: `${err.toString()}`}];
+}
+return null;
+"""
+OPTIONS(
+  library = ["gs://cdn.polkaholic.io/pako.min.js", "gs://cdn.polkaholic.io/colorfulnotion-decoderData.js", "gs://blockchain-etl-bigquery/ethers-v5.js"]
+);
+
+
--- sql/PARSE_EVENT.sql
+++ sql/PARSE_EVENT.sql
@@ -1,50 +1,128 @@
-CREATE OR REPLACE FUNCTION `abi_functions`.PARSE_EVENT(abi STRING, functionname STRING, logtopics ARRAY<STRING>, logdata STRING) RETURNS ARRAY<STRUCT<arg STRING, val STRING>> LANGUAGE js AS """
-abi = JSON.parse(abi);
-    var interface_instance = new ethers.utils.Interface(abi);
-    // PunkBidEntered (index_topic_1 uint256 punkIndex, uint256 value, index_topic_2 address fromAddress)
-    // PunkBidEntered(uint256,uint256,address)
-    // JSON.stringify(pt)
-    // var topicID = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('PunkBidEntered(uint256,uint256,address)'));
-    // {"anonymous":false,"inputs":[{"indexed":true,"name":"punkIndex","type":"uint256"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":true,"name":"fromAddress","type":"address"}],"name":"PunkBidEntered","type":"event"}
-
-    var stripZeros = /^0x0+/;
-    var stripAddress = /^0x0{24}/;
-
-    txargs = [];
-    txtypes = [];
-    res = [];
-
-    // try-catch - input may be malformed.
-    try {
-//      var pt = interface_instance.parseTransaction({data: dat});
-
-      frag = "";
-      abi.forEach(function(x){
-        if (x.name == functionname && x.type == "event") {
-          frag = x;
-        }
-      });
-
-      j=1;
-      for (i=0; i < frag.inputs.length; i++) {
-        typ = frag.inputs[i].type;
-        if (frag.inputs[i].indexed) {
-          val = logtopics[j];
-          j++;
-        }
-        else {
-          val = logdata
-        }
-        if (typ == "uint256") {
-          val = Number(val.replace(stripZeros, '0x'));
-        }
-        else if (typ == "address") {
-          val = val.toLowerCase();
-          val = val.replace(stripAddress, '0x');
-        }
-        res.push({arg:frag.inputs[i].name,val:val});
-      }
-    } catch(e) {}
-    return res;
+CREATE OR REPLACE FUNCTION `substrate-etl.abi_functions.PARSE_EVENT`(data STRING, topics ARRAY<STRING>) 
+RETURNS ARRAY<STRUCT<name STRING, value STRING>> 
+LANGUAGE js AS 
 """
-OPTIONS(library="gs://blockchain-etl-bigquery/ethers.js");
+function generateBinaryNumbers(N, k) {
+  const result = [];
+  function generateCombinations(n, k, prefix = '') {
+    if (k === 0) {
+      result.push(prefix.padStart(N, '0'));
+      return;
+    }
+    if (n === 0) return;
+    generateCombinations(n - 1, k - 1, '1' + prefix);
+    generateCombinations(n - 1, k, '0'+ prefix);
+  }
+  generateCombinations(N, k);
+  return result;
+}
+function convertBN(val) {
+    if (Array.isArray(val)) {
+        for (let i = 0; i < val.length; i++) {
+            val[i] = convertBN(val[i])
+	}
+    } else if ( val._isBigNumber ) {
+	return val.toString();
+    } 
+    return val;
+}
+
+let abiCandidates = [];
+try {
+    let abi_maybe = null;
+    let text_signature = null;
+    let abiencoded = cn_events[topics[0].substring(0,10)];
+    if ( abiencoded ) {
+      let pieces = abiencoded.split("|");
+      if ( pieces.length == 4 ) {
+        let eventName = pieces[0];
+        let abi = {"name":eventName,"type":"event","inputs":[], "outputs":[], "stateMutability":"nonpayable"};
+        let flds = pieces[1].length > 0 ? pieces[1].split(",") : [];
+        let types_unindexed = pieces[2].length > 0 ? pieces[2].split(",") : [];
+        let types_indexed = pieces[3].length > 0 ? pieces[3].split(",") : [];
+        let types = [];
+        let u = 0;
+        let i = 0;
+        for (let idx = 0; idx < flds.length; idx++) {
+           let f = flds[idx];
+	   let lastchar = f.substring(f.length-1, f.length);
+	   if ( lastchar == "*" ) {
+	     abi.inputs.push({"indexed": true, name: f.substring(0, f.length-1), type: types_indexed[i] });
+             types.push(types_indexed[i]);  
+	     i++;
+	   } else {
+	     abi.inputs.push({"indexed": false, name: f, type: types_unindexed[u] });
+             types.push(types_unindexed[u]);  
+	     u++;
+	   }
+        }
+        abi_maybe = JSON.stringify(abi);
+        text_signature = `${eventName}(${types.join(",")})`
+      }
+    } else {
+      return null;
+    }
+    let abiMaybe = JSON.parse(abi_maybe);
+    let N = abiMaybe.inputs.length; 
+    let topicLen = 0;
+    abiMaybe.inputs.forEach(function(x) {
+	if ( x.indexed ) topicLen++;
+    });
+    if ( topics.length != topicLen + 1 ) {
+	if ( topics.length == 1 || topics.length == N + 1) { 
+	    let v = topics.length == 1 ? false : true;
+	    let cand = JSON.parse(abi_maybe);
+	    for (let i = 0; i < N; i++) {
+		cand.inputs[i].indexed = v;
+	    }
+	    abiCandidates.push(cand);
+	} else if ( (topics.length == 2 || topics.length == N ) ){  
+	    let v = ( topics.length == 2 ) ? true : false;
+	    for ( let i = 0; i < N; i++) {
+		let cand = JSON.parse(abi_maybe);
+		for (let j = 0; j < N; j++) {
+		    cand.inputs[j].indexed = ( i == j ) ? v : ! v;
+		}
+		abiCandidates.push(cand);
+	    }
+	} else { 
+	    let binaryStrings = generateBinaryNumbers(N, topics.length - 1);
+	    for ( const b of binaryStrings ) {
+      		let cand = JSON.parse(abi_maybe);
+		for (let j = 0; j < b.length; j++) {
+		    cand.inputs[j].indexed = ( b[j] == "1" ) ? true : false;
+		}
+		abiCandidates.push(cand);
+	    }
+	}
+	
+    } else {
+	abiCandidates.push(abiMaybe);
+    }
+    let tries = 0;
+    for (const abiCandidate of abiCandidates) {
+	try {
+	    const iface = new ethers.utils.Interface([abiCandidate]);
+	    const decodedData = iface.decodeEventLog(ethers.utils.id(text_signature), data, topics);
+	    let res = abiCandidate.inputs.map(function(y, idx){
+		let v = decodedData[idx];
+		return({name: y.name, value: convertBN(v) });
+	    });
+	    if ( tries > 0 ) {
+		res.push({"name": "tries", "value": tries});
+	    }
+	    return res;
+	} catch ( err ) {
+	    tries++;
+	}
+    }
+} catch (err) {
+    return [{name: "err", value: `${err.toString()}`}];
+}
+
+return [{name: "err", value: `decode failure`}];
+"""
+OPTIONS(
+  library = ["gs://cdn.polkaholic.io/pako.min.js", "gs://cdn.polkaholic.io/colorfulnotion-decoderData.js", "gs://blockchain-etl-bigquery/ethers-v5.js"]
+);
+
